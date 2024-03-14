0ac9d349a73c28e864ca22b307f516f425fda8c9
Mowowglii <erwann.razatovo@gmail.com>
2024-03-14 18:45:33 +0100

    Sudoku.py first widgets

--- Sudoku.py
+++ Sudoku.py
@@ -1,8 +1,34 @@
 import tkinter as tk
 
+#Root
 root=tk.Tk()
 root.title("Sudoku Game !")
 root.resizable(False, False)
+
+#First Frame on root
 menuframe=tk.Frame(root, width=250, height=200)
 menuframe.grid()
+menuframe.propagate(False)
+for c in range(3):
+    menuframe.grid_columnconfigure(c, weight=1)
+for r in range(5):
+    menuframe.grid_rowconfigure(r, weight=1)
+#Widgets
+#Widgets on menu frame
+titlemenu=tk.Label(menuframe, text="Sudoku Game !", font=("tahoma", 14, "bold"))
+newgb=tk.Button(menuframe, text="New Game", padx=5, pady=5)
+loadgb=tk.Button(menuframe, text="Load Game", padx=5, pady=5)
+playoldb=tk.Button(menuframe, text="Play Old Ones", padx=5, pady=5)
+quitb=tk.Button(menuframe, text="Quit", command=root.destroy, padx=5, pady=5)
+menuloc=tk.Label(menuframe, text="Menu", font=("tahoma", 8, "italic"))
+
+#Display
+#Widgets on menu frame
+titlemenu.grid(row=0, column=1, columnspan=2)
+newgb.grid(row=1, column=1, pady=5)
+loadgb.grid(row=2 ,column=1, pady=5)
+playoldb.grid(row=3, column=1, pady=5)
+quitb.grid(row=4, column=0, padx=10, pady=10)
+menuloc.grid(row=4, column=2, sticky="SE")
+
 root.mainloop()
