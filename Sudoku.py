a104dc3cd8cf47d02790b0fb3ed834fb69f3fe66
Mowowglii <erwann.razatovo@gmail.com>
2024-03-14 19:21:35 +0100

    Sudoku.py more widgets

--- Sudoku.py
+++ Sudoku.py
@@ -1,21 +1,24 @@
 import tkinter as tk
 
+#Functions
+
 #Root
 root=tk.Tk()
-root.title("Sudoku Game !")
+root.title("Sudoku Game ")
 root.resizable(False, False)
 
 #First Frame on root
 menuframe=tk.Frame(root, width=250, height=200)
-menuframe.grid()
-menuframe.propagate(False)
+menuframe.grid_propagate(False)
 for c in range(3):
     menuframe.grid_columnconfigure(c, weight=1)
 for r in range(5):
     menuframe.grid_rowconfigure(r, weight=1)
+menuframe.grid()
+
 #Widgets
 #Widgets on menu frame
-titlemenu=tk.Label(menuframe, text="Sudoku Game !", font=("tahoma", 14, "bold"))
+titlemenu=tk.Label(menuframe, text="Sudoku Game ", font=("tahoma", 14, "bold"), anchor="center")
 newgb=tk.Button(menuframe, text="New Game", padx=5, pady=5)
 loadgb=tk.Button(menuframe, text="Load Game", padx=5, pady=5)
 playoldb=tk.Button(menuframe, text="Play Old Ones", padx=5, pady=5)
@@ -24,7 +27,7 @@ menuloc=tk.Label(menuframe, text="Menu", font=("tahoma", 8, "italic"))
 
 #Display
 #Widgets on menu frame
-titlemenu.grid(row=0, column=1, columnspan=2)
+titlemenu.grid(row=0, column=1, pady=5)
 newgb.grid(row=1, column=1, pady=5)
 loadgb.grid(row=2 ,column=1, pady=5)
 playoldb.grid(row=3, column=1, pady=5)
