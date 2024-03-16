b83b240857136b55e23e8de60d6e2e726be14e1d
Mowowglii <erwann.razatovo@gmail.com>
2024-03-16 17:54:53 +0100

    global variable suernumber created, caseindex_to_casepx, mouse_to_case, usern_selection functions created, select grid modification, playgrid bug corrected

--- Sudoku.py
+++ Sudoku.py
@@ -1,8 +1,28 @@
 import tkinter as tk
 
+#Global Variables
+usernumber=0
+
 #Functions
-def px_to_index(x_pos:int, y_pos:int, row:int, column:int, canwidth:int, canheight:int, canborder=0)->list[int,int]:
-    """Convert Pix Position in Canvas to index in grid
+def caseindex_to_casepx(x_pos:int, y_pos:int, row:int, column:int, canwidth:int, canheight:int, canborder=0)->list[int,int]:
+    """Convert Case Index position to Case Pixel position in canva
+
+    Args:
+        x_pos (int): Index x
+        y_pos (int): Index y
+        row (int): Row of the canva
+        column (int): Column of the canva
+        canwidth (int): Width of canva
+        canheight (int): Height of canva
+        canborder (int, optional): Canva border. Defaults to 0.
+
+    Returns:
+        list[int,int]: [x positon in px, y position in px]
+    """
+    return [int(x_pos*((canwidth+canborder)/column)), int(y_pos*((canheight+canborder)/row))]
+
+def mouse_to_case(x_pos:int, y_pos:int, row:int, column:int, canwidth:int, canheight:int, canborder=0)->list[int,int]:
+    """Convert Mouse Pix Position in Canvas to Case index in grid
 
     Args:
         x_pos (int): Position x
@@ -47,17 +67,19 @@ def playgridS(event=tk.Event):
     Args:
         event (tk.Event, optional): The event generated (here is click)
     """
-    print(f'Click on Playgrid : {px_to_index(event.x, event.y, 9, 9, 702, 702, 7)}')
-    
-def selectcanvS(event=tk.Event):
-    """The goal of this function is to lock user number selection from clicking in the selection grid
+    print(f'Click on Playgrid : {mouse_to_case(event.x, event.y, 9, 9, 702, 702, 7)}')
+
+def usern_selection(number:int):
+    """Set User Selected number
 
     Args:
-        event (tk.Event, optional): The event generated (here is click)
+        number (int): The chosen number
     """
-    print(f'Click in Selectgrid : {px_to_index(event.x, event.y, 3, 3, 225, 225, 7)}')
+    global usernumber
+    usernumber=number
+    print(usernumber)
     
-def newg_closed():
+def game_window_closed():
     """This function put menu back and destroy newg_window
     """
     global root
@@ -69,6 +91,10 @@ def game_window():
     """Create a Sudoku Game Window"""
     global root
     global new_game_window
+    global selectbcanv
+    global extracanv
+    global playcanv
+    global usernumber
     root.iconify()
     new_game_window=tk.Toplevel(root)
     #New Game Window
@@ -79,11 +105,19 @@ def game_window():
     newg_frame=tk.Frame(new_game_window, width=1000, height=750)
     newg_frame.grid_propagate(False)
     newg_frame.grid()
-
+    
     #Widgets on Frame
     playcanv=tk.Canvas(newg_frame, width=702, height=702, borderwidth=5, relief="sunken")
     selectbcanv=tk.Canvas(newg_frame, width=225, height=225, borderwidth=5, relief="sunken")
     extracanv=tk.Canvas(newg_frame, width=225, height=225, borderwidth=5, relief="ridge")
+
+    #Playcanv configuration
+    playcanv.grid_propagate(False)
+    playcanv.grid_propagate(False)
+    for c in range(9):
+        playcanv.grid_columnconfigure(c, weight=1)
+    for r in range(9):
+        playcanv.grid_rowconfigure(r, weight=1)
     
     #Extra Canva Configuration
     extracanv.grid_propagate(False)
@@ -101,39 +135,39 @@ def game_window():
     for r in range(3):
         selectbcanv.grid_rowconfigure(r, weight=1)
     
-    #Slicing Play Canva for Grid Display
+    #Slicing Play Canv
     slicecanvas(playcanv, 9, 9, 702, 702)
     
-    #Binding Play Canva Grid
-    playcanv.bind('<ButtonPress-1>', playgridS)
-        
-    #Slicing Select Canva
-    slicecanvas(selectbcanv, 3, 3, 225, 225)
+    #Widget on Play Canv
+    gridframe=tk.Frame(playcanv, width=702, height=702, bg="")
     
-    #Binding Select Canva Grid
-    selectbcanv.bind('<ButtonPress-1>', selectcanvS)
+    #Display on Play Canv
+    gridframe.grid(row=0, column=0, columnspan=9, rowspan=9)
+    
+    #Binding Play Canva Grid
+    gridframe.bind('<ButtonPress-1>', playgridS)
     
     #widgets in Select Canva
-    label1=tk.Label(selectbcanv, text="1", font=("ClearSans"))
-    label2=tk.Label(selectbcanv, text="2", font=("ClearSans"))
-    label3=tk.Label(selectbcanv, text="3", font=("ClearSans"))
-    label4=tk.Label(selectbcanv, text="4", font=("ClearSans"))
-    label5=tk.Label(selectbcanv, text="5", font=("ClearSans"))
-    label6=tk.Label(selectbcanv, text="6", font=("ClearSans"))
-    label7=tk.Label(selectbcanv, text="7", font=("ClearSans"))
-    label8=tk.Label(selectbcanv, text="8", font=("ClearSans"))
-    label9=tk.Label(selectbcanv, text="9", font=("ClearSans"))
+    b1=tk.Button(selectbcanv, text="1", font=("ClearSans"), relief="groove", padx=15, pady=10, command=lambda: usern_selection(1))
+    b2=tk.Button(selectbcanv, text="2", font=("ClearSans"), relief="groove", padx=15, pady=10, command=lambda: usern_selection(2))
+    b3=tk.Button(selectbcanv, text="3", font=("ClearSans"), relief="groove", padx=15, pady=10, command=lambda: usern_selection(3))
+    b4=tk.Button(selectbcanv, text="4", font=("ClearSans"), relief="groove", padx=15, pady=10, command=lambda: usern_selection(4))
+    b5=tk.Button(selectbcanv, text="5", font=("ClearSans"), relief="groove", padx=15, pady=10, command=lambda: usern_selection(5))
+    b6=tk.Button(selectbcanv, text="6", font=("ClearSans"), relief="groove", padx=15, pady=10, command=lambda: usern_selection(6))
+    b7=tk.Button(selectbcanv, text="7", font=("ClearSans"), relief="groove", padx=15, pady=10, command=lambda: usern_selection(7))
+    b8=tk.Button(selectbcanv, text="8", font=("ClearSans"), relief="groove", padx=15, pady=10, command=lambda: usern_selection(8))
+    b9=tk.Button(selectbcanv, text="9", font=("ClearSans"), relief="groove", padx=15, pady=10, command=lambda: usern_selection(9))
     
     #Display on Select Canva Frame
-    label1.grid(row=0, column=0)
-    label2.grid(row=0, column=1)
-    label3.grid(row=0, column=2)
-    label4.grid(row=1, column=0)
-    label5.grid(row=1, column=1)
-    label6.grid(row=1, column=2)
-    label7.grid(row=2, column=0)
-    label8.grid(row=2, column=1)
-    label9.grid(row=2, column=2)
+    b1.grid(row=0, column=0)
+    b2.grid(row=0, column=1)
+    b3.grid(row=0, column=2)
+    b4.grid(row=1, column=0)
+    b5.grid(row=1, column=1)
+    b6.grid(row=1, column=2)
+    b7.grid(row=2, column=0)
+    b8.grid(row=2, column=1)
+    b9.grid(row=2, column=2)
     
     #Widgets in Extra Canva
     nbutton=tk.Checkbutton(extracanv, text="Note Mode", font=("CleanSans",10), relief="groove")
@@ -142,8 +176,8 @@ def game_window():
     qbutton=tk.Button(extracanv, text="Give up", font=("CleanSans", 10), relief="groove", command=new_game_window.destroy)
     namelabel=tk.Label(extracanv, text="Grid Number", font=("CleanSans", 16, "bold"))
     difficlabel=tk.Label(extracanv, text="Difficulty", font=("CleanSans", 14, "bold"))
-    timelabel=tk.Label(extracanv, text="Timer", font=("ClearSans", 10))
-    errorlabel=tk.Label(extracanv, text="Error n°", font=("CleanSans", 10))
+    timelabel=tk.Label(extracanv, text="00:00", font=("ClearSans", 10))
+    errorlabel=tk.Label(extracanv, text="x errors", font=("CleanSans", 10))
     
     #Display on Extra Canva
     ebutton.grid(row=3, column=0)
@@ -160,8 +194,8 @@ def game_window():
     selectbcanv.grid(row=1, column=1, padx=10, pady=10)
     extracanv.grid(row=0, column=1, padx=10, pady=10)
     
-    #When newg_window is closed
-    new_game_window.protocol("WM_DELETE_WINDOW", newg_closed)
+    #When game_window is closed
+    new_game_window.protocol("WM_DELETE_WINDOW", game_window_closed)
 
 #Root
 root=tk.Tk()
@@ -194,6 +228,5 @@ loadgb.grid(row=2 ,column=1, pady=5)
 playoldb.grid(row=3, column=1, pady=5)
 quitb.grid(row=4, column=0, padx=10, pady=10)
 menuloc.grid(row=4, column=2, sticky="SE")
-
 
 root.mainloop()
